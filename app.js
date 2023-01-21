2f25b47004393c56bea59149f206adbf2f25a066
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-01-21 02:36:06 +0100

    finding fruits

--- app.js
+++ app.js
@@ -13,6 +13,49 @@ const fruit = new Fruit({
     name: "Apple",
     ratings: 7,
     review: "Not bad for a common apple."
-})
+});
 
-fruit.save().then(() => console.log("Saved new fruits"));
+const banana = new Fruit({
+    name: "Banana",
+    ratings: 6,
+    review: "Love eating them."
+});
+
+const pawpaw = new Fruit({
+    name: "Pawpaw",
+    ratings: 7,
+    review: "Absolutely lovely fruits."
+});
+
+// Fruit.insertMany([banana, pawpaw], (err) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+//         console.log("fruits added successfully!");
+//     }
+// });
+
+//fruit.save().then(() => console.log("Saved new fruits"));
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
+
+const Person = new mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John Cena",
+    age: 37
+});
+
+//person.save().then(() => console.log("New person added to DB."));
+
+
+Fruit.find((err, fruits) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(fruits);
+    }
+});
